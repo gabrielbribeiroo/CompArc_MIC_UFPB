e4fc77f8c2118c31458ea8e8950ac58d0fda113d
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-10 15:53:46 -0300

    optimization code

--- multiplication.asm
+++ multiplication.asm
@@ -1,18 +1,22 @@
 MUL:
-    MAR <- SP, RD            // Load first operand (multiplicand)
-    H <- MDR                 // Store in register H
-    MAR <- SP + 1, RD        // Load second operand (multiplier)
-    MDR <- MDR - 1           // Adjust iteration counter
-    MAR <- SP, WR            // Zero the stack position to store the result
-    MDR <- 0
-    MAR <- SP, WR
+    MAR <- SP, RD            ; Load the first operand (multiplicand)
+    H <- MDR                 ; Store it in register H
+    MAR <- SP + 1, RD        ; Load the second operand (multiplier)
+    MDR <- MDR               ; Ensure the multiplier is in MDR
+    MAR <- SP + 1, WR        ; Save the multiplier back to the stack
+    MDR <- 0                 ; Initialize the accumulator to 0
+    MAR <- SP, WR            ; Store the accumulator in the stack
+
 MUL_LOOP:
-    IF MDR < 0 THEN GOTO END_MUL  // If the multiplier is zero, end
-    MAR <- SP, RD            // Get the accumulated value
-    MDR <- MDR + H           // Add the multiplicand to the accumulator
-    MAR <- SP, WR            // Store new accumulated value
-    MAR <- SP + 1, RD        // Get multiplier
-    MDR <- MDR - 1           // Decrement multiplier
-    GOTO MUL_LOOP            // Repeat until the multiplier is zero
+    MAR <- SP + 1, RD        ; Retrieve the multiplier
+    IF MDR == 0 THEN GOTO END_MUL  ; If the multiplier is zero, exit loop
+    MAR <- SP, RD            ; Retrieve the accumulated value
+    MDR <- MDR + H           ; Add the multiplicand to the accumulator
+    MAR <- SP, WR            ; Store the updated accumulated value
+    MAR <- SP + 1, RD        ; Retrieve the multiplier again
+    MDR <- MDR - 1           ; Decrease the multiplier by 1
+    MAR <- SP + 1, WR        ; Store the updated multiplier back on the stack
+    GOTO MUL_LOOP            ; Repeat until the multiplier reaches zero
+
 END_MUL:
-    PC <- PC + 1, FETCH      // Next instruction
+    PC <- PC + 1, FETCH      ; Move to the next instruction
