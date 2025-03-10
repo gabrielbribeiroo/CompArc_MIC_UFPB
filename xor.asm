8a0065d4f8150fcb998479302e4b9d0bd70701f0
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-10 15:55:14 -0300

    code efficiently performs the XOR operation in MIC-1

--- xor.asm
+++ xor.asm
@@ -1,7 +1,7 @@
 XOR:
-    MAR <- SP, RD            // Load first operand from the top of the stack
-    H <- MDR                 // Store in register H
-    MAR <- SP + 1, RD        // Load second operand
-    MDR <- H XOR MDR         // Perform XOR between operands
-    MAR <- SP, WR            // Store the result at the top of the stack
-    PC <- PC + 1, FETCH      // Next instruction
+    MAR <- SP, RD            ; Load first operand from the top of the stack
+    H <- MDR                 ; Store it in register H
+    MAR <- SP + 1, RD        ; Load second operand
+    MDR <- H XOR MDR         ; Perform XOR between operands using the ALU
+    MAR <- SP, WR            ; Store the result at the top of the stack
+    PC <- PC + 1, FETCH      ; Fetch the next instruction
