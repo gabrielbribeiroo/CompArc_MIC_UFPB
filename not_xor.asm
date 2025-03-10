d45ec5c3f3e62072644c052a9ca9c6fb059be929
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-10 15:56:15 -0300

    correcting code

--- not_xor.asm
+++ not_xor.asm
@@ -1,8 +1,10 @@
 NOT_XOR:
-    MAR <- SP, RD            // Load first operand from the top of the stack
-    H <- MDR                 // Store in register H
-    MAR <- SP + 1, RD        // Load second operand
-    MDR <- H XOR MDR         // Perform XOR between operands
-    MDR <- NOT MDR           // Invert XOR result (negation)
-    MAR <- SP, WR            // Store the result at the top of the stack
-    PC <- PC + 1, FETCH      // Next instruction
+    MAR <- SP, RD            ; Load first operand from the top of the stack
+    H <- MDR                 ; Store the first operand in register H
+    MAR <- SP + 1, RD        ; Load second operand from the stack
+    MDR <- H AND MDR         ; Perform AND between the operands
+    MAR <- SP + 2, RD        ; Get OR operand
+    MDR <- MDR OR H          ; Perform OR operation
+    MDR <- NOT MDR           ; Invert the XOR result (negation)
+    MAR <- SP, WR            ; Store the negated result back at the top of the stack
+    PC <- PC + 1, FETCH      ; Fetch the next instruction
