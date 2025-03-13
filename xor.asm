fc154f31918db5b2c6ea23e01cd70e6c62be2b83
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-13 20:50:34 -0300

    adjust code to MIC-1

--- xor.asm
+++ xor.asm
@@ -1,7 +1,14 @@
 XOR:
-    MAR <- SP, RD            ; Load first operand from the top of the stack
-    H <- MDR                 ; Store it in register H
-    MAR <- SP + 1, RD        ; Load second operand
-    MDR <- H XOR MDR         ; Perform XOR between operands using the ALU
-    MAR <- SP, WR            ; Store the result at the top of the stack
+    MAR <- SP, RD            ; Load first operand (A) from the stack
+    H <- MDR                 ; Store operand A in register H
+    MAR <- SP + 1, RD        ; Load second operand (B)
+    OPC <- MDR               ; Store operand B in OPC
+
+    H <- H OR OPC            ; Compute (A OR B), store in H
+    AC <- H                  ; Save (A OR B) in AC
+    H <- MDR AND OPC         ; Compute (A AND B)
+    H <- NOT H               ; Compute NOT (A AND B)
+    MDR <- AC AND H          ; Compute (A OR B) AND (NOT (A AND B)), result is A XOR B
+
+    MAR <- SP, WR            ; Store the XOR result back at the top of the stack
     PC <- PC + 1, FETCH      ; Fetch the next instruction
