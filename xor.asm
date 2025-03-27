8dcd1dc3901683f3a096922deffaef394e2e3242
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-26 21:21:01 -0300

    refactor XOR operation for clarity and improved operand handling

--- xor.asm
+++ xor.asm
@@ -1,23 +1,20 @@
 XOR:
-    H = TOS                 ; Load top of stack (A) into H
-    MAR = H; rd             ; Read the value from memory
-    PC = PC + 1             ; Increment PC
-    OPC = MDR               ; Store operand A in OPC
-    
-    H = SP - 1              ; Get second operand (B) from SP-1
-    MAR = H; rd             ; Read the value from memory
-    PC = PC + 1             ; Increment PC
-    H = MDR                 ; Store operand B in H
-    
-    H = OPC OR H            ; Compute (A OR B)
-    AC = H                  ; Save (A OR B) in AC
-    H = OPC AND H           ; Compute (A AND B)
-    H = NOT H               ; Compute NOT (A AND B)
-    AC = AC AND H           ; Compute (A OR B) AND (NOT (A AND B)), result is A XOR B
-    
-    H = SP - 1              ; Get SP-1 to store result
-    MAR = H                 ; Prepare to write result
-    MDR = AC; wr            ; Write XOR result to memory
-    SP = SP - 1             ; Adjust stack pointer
-    PC = PC + 1             ; Move to the next instruction
-    goto Main1              ; Return to main program
+    H = TOS                ; Load the first operand (A) from the top of the stack
+    MAR = H; rd            ; Read the value from memory into MDR
+    PC = PC + 1;           ; Increment the program counter
+    OPC = MDR              ; Store operand A in OPC
+    H = SP - 1             ; Get the second operand (B) from SP - 1
+    MAR = H; rd            ; Read operand B from memory
+    PC = PC + 1;           ; Increment the program counter
+    H = MDR                ; Store operand B in H
+
+    H = OPC OR H           ; Compute (A OR B) and store it in H
+    AC = H                 ; Store (A OR B) in AC
+    H = OPC AND MDR        ; Compute (A AND B) and store it in H
+    H = NOT H              ; Compute NOT (A AND B)
+    MDR = AC AND H         ; Compute (A OR B) AND (NOT (A AND B)), which is A XOR B
+
+    H = SP                 ; Get the stack pointer position
+    MAR = H                ; Set the memory address for storing the result
+    MDR = MDR; wr          ; Write the XOR result to memory
+    Goto Main1             ; Return to the main programs
