f68893a294e5e90b6fcdd3847f82f9aef4f7e6a4
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-04-21 11:39:03 -0300

    refactor XOR routine for improved operand handling and clarity

--- xor.asm
+++ xor.asm
@@ -1,20 +1,15 @@
 XOR:
-    H = TOS                ; Load the first operand (A) from the top of the stack
-    MAR = H; rd            ; Read the value from memory into MDR
-    PC = PC + 1;           ; Increment the program counter
-    OPC = MDR              ; Store operand A in OPC
-    H = SP - 1             ; Get the second operand (B) from SP - 1
-    MAR = H; rd            ; Read operand B from memory
-    PC = PC + 1;           ; Increment the program counter
-    H = MDR                ; Store operand B in H
+    OPC = TOS                ; Load the first operand (A) from the top of the stack
+    SP = SP - 1              ; Move the stack pointer to get the second operand (B)
+    MAR = SP; RD             ; Read the second operand (B) from memory
+    PC = PC + 1              ; Increment the program counter
+    H = MDR                  ; Store the second operand (B) in H
 
-    H = OPC OR H           ; Compute (A OR B) and store it in H
-    AC = H                 ; Store (A OR B) in AC
-    H = OPC AND MDR        ; Compute (A AND B) and store it in H
-    H = NOT H              ; Compute NOT (A AND B)
-    MDR = AC AND H         ; Compute (A OR B) AND (NOT (A AND B)), which is A XOR B
+    H = OPC OR H             ; Compute A OR B and store in H
+    AC = H                   ; Save (A OR B) in AC
+    H = OPC AND MDR          ; Compute A AND B and store in H
+    H = NOT H                ; Compute NOT (A AND B)
+    MDR = AC AND H           ; Compute (A OR B) AND (NOT (A AND B)) = A XOR B
 
-    H = SP                 ; Get the stack pointer position
-    MAR = H                ; Set the memory address for storing the result
-    MDR = MDR; wr          ; Write the XOR result to memory
-    Goto Main1             ; Return to the main programs
+    TOS = MDR; WR            ; Write the result back to the top of the stack
+    goto Main1               ; Return to the main program
